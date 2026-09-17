c21ad6565eadbb733beddb0cf3bb8190b5778e8f
Mjelectrachapel <mj.ruizredondo1@gmail.com>
2026-09-17 10:11:03 +0200

    Ejercicio 1 DOM

---
+++ ejercicios_DOM/ejercicio_1/script.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector(".showme"));
+console.log(document.querySelector("#pillado"));
+console.log(document.querySelectorAll("p"));
+console.log(document.querySelectorAll(".pokemon"));
+console.log(document.querySelectorAll('[data-function="testMe"]'));
+console.log(document.querySelectorAll('[data-function="testMe"]')[2]);
